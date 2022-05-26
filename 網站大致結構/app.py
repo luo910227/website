4cd0f6bfb463a6aa99eee4252788e39a2e2c401f
mow <a20011010a@gmail.com>
2022-05-26 22:27:00 +0800

    1

---
+++ 網站大致結構/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, url_for
+from numpy import true_divide
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/index.html')
+def index1():
+    return render_template('index.html')
+
+@app.route('/bot.html')
+def index2():
+    return render_template('bot.html')
+
+@app.route('/portfolio-details.html')
+def index3():
+    return render_template('portfolio-details.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+        
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000, debug=True)
+
+
