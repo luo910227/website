dbba13e0b66f599c9cffbf6a37e22dae12a96953
luo910227 <luo910227@gmail.com>
2022-06-07 11:51:43 +0800

    ok

--- app.py
+++ app.py
@@ -19,6 +19,6 @@ def page_not_found(e):
     return render_template('500.html'), 500
         
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5009, debug=True)
 
 
